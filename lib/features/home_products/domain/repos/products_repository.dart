69df2d1abe83ffed3bf9c423de7f913798eb0030
elkholy <boodykh@gmail.com>
2024-11-24 20:46:05 +0200

    refactor products repository

--- lib/features/home_products/domain/repos/products_repository.dart
+++ lib/features/home_products/domain/repos/products_repository.dart
@@ -1,9 +1,7 @@
-import 'package:dartz/dartz.dart';
-import 'package:we_shop/core/failure/failure.dart';
 import 'package:we_shop/features/home_products/domain/entities/Product.dart';
 
 abstract class ProductsRepository {
-  Future<Either<Failure, List<Product>>> getMainProducts();
-  Future<Either<Failure, List<Product>>> getMostUsedProducts();
-  Future<Either<Failure, List<Product>>> getRecommendedProducts();
+  Future<List<Product>> getMainProducts();
+  Future<List<Product>> getMostUsedProducts();
+  Future<List<Product>> getRecommendedProducts();
 }
