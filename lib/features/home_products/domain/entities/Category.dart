92f8e2d078d09544eeb792bee918b456e23280ba
elkholy <boodykh@gmail.com>
2024-11-24 20:00:05 +0200

    add product entity

---
+++ lib/features/home_products/domain/entities/Category.dart
@@ -0,0 +1,15 @@
+class Category {
+  int? id;
+  String? name;
+  String? image;
+  String? creationAt;
+  String? updatedAt;
+
+  Category({
+    this.id,
+    this.name,
+    this.image,
+    this.creationAt,
+    this.updatedAt,
+  });
+}
---
+++ lib/features/home_products/domain/entities/Product.dart
@@ -0,0 +1,23 @@
+import 'Category.dart';
+
+class Product {
+  Product({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.images,
+    this.creationAt,
+    this.updatedAt,
+    this.category,
+  });
+
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  List<String>? images;
+  String? creationAt;
+  String? updatedAt;
+  Category? category;
+}
