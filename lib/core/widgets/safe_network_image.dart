7006aeed503e701821d325f465cbddcb688b9cc4
elkholy <boodykh@gmail.com>
2024-11-25 20:59:44 +0200

    added safe network image core widget

---
+++ lib/core/widgets/safe_network_image.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:we_shop/core/constants/colors.dart';
+import 'package:we_shop/core/widgets/loading_skeletons/shimmer_wrapper.dart';
+
+class SafeNetworkImage extends StatelessWidget {
+  final String url;
+  final double? width;
+  final double? height;
+  const SafeNetworkImage(
+      {super.key, required this.url, this.width, this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: url,
+      width: width,
+      height: height,
+      placeholder: (context, url) => ShimmerWrapper(
+        child: SizedBox(
+          width: width,
+          height: height,
+        ),
+      ),
+      errorWidget: (context, url, error) => const Center(
+        child: Icon(
+          Icons.error_outline,
+          color: AppColors.lightGrey,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
